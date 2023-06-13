decadb876579827f718ace3257bc447daa983790
Maryam <akhlaghi.72.m@gmail.com>
2023-06-13 22:02:24 +0330

    imported axios and  set result for current weather

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Weather.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import BeatLoader from "react-spinners/BeatLoader";
+import axios from "axios";
 
 export default function Weather(){
     const override= {
@@ -9,7 +10,20 @@ export default function Weather(){
         margin: "0 auto",
         
       };
-    const [loaded, setLoaded] = useState(false);
+    let [loaded, setLoaded] = useState(false);
+    let [weatherData, setWeatherData] =useState(null);
+    function setResult(response){
+        setWeatherData({city : response.data.city ,
+             country : response.data.country ,
+              description : response.data.condition.description,
+              humidity : Math.round(response.data.temperature.humidity) ,
+              wind : Math.round(response.data.wind.speed),
+              temp : Math.round(response.data.temperature.current),
+              icon : response.data.condition.icon,
+              
+             })
+       setLoaded(true);
+    };
     if (loaded){
     return <div className='Weather'>
         <div className="container">
@@ -27,19 +41,22 @@ export default function Weather(){
             <div className="info-container">
                 
               <div>
-                 <h1>Los Angeles</h1>
-                 <p>Sunday 12:25 ,<span> Broken Clouds</span></p>
-                 <p>Humidity :<span className="colored-numbers"> 88%</span><span>, Wind : <span className="colored-numbers">4.63km/h</span></span></p>
+                 <h1>{weatherData.city}, {weatherData.country}</h1>
+                 <p>Sunday 12:25 ,<span> {weatherData.description}</span></p>
+                 <p>Humidity :<span className="colored-numbers"> {weatherData.humidity}%</span><span>, Wind : <span className="colored-numbers">{weatherData.wind}km/h</span></span></p>
               </div>
               <div>
-                 <img src="./img.png" alt="description"></img> <span className="temperature">12</span><span className="unit">°C</span>
+              <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/{weatherData.icon}.png" alt={weatherData.description} /> <span className="temperature">{weatherData.temp}</span><span className="unit">°C</span>
 
               </div>
             </div>
         </div>
     </div>}
     else{
-
+        let city ="Sydney"
+        const apiKey="8bd8249b0de0430coc410dff16ctaab6";
+        let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(setResult);
         return <div className="Weather">
            <div className="loading">Loading  😊  {" "}
            <BeatLoader color="#F65282"
